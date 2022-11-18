a147d933dd51e2a050002e85f96842f7cca6248f
Tsz Fung Tam <petertam0712@hotmail.com>
2022-11-18 17:04:57 +0800

    exercise 1-3 done

--- src/RandomWord.java
+++ src/RandomWord.java
@@ -1,22 +1,20 @@
-import edu.princeton.cs.algs4.*;
-
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
 public class RandomWord {
     public static void main(String[] args) {
         String word, champion = null;
-        int i = 0;
 
-        while(!StdIn.isEmpty()){
-            i++;
+        for (int i = 1; !StdIn.isEmpty(); i++) {
+
             word = StdIn.readString();
-            if (StdRandom.bernoulli(1/i){
+            if (StdRandom.bernoulli(1 / i)) {
                 champion = word;
             }
         }
         StdOut.println(champion);
         }
-    }
-
-
+}
 
 
 
